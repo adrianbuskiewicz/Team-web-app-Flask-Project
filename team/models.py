32dbb9575c13c07a9aa7d3755eabfb5b38c43307
Adrian Buśkiewicz <adrian.k.buskiewicz@gmail.com>
2021-08-02 11:46:45 +0200

    Changed models schema
    I changed all models and created a new models of meeting. Made a profile model which will belong one to one user (if user isn't coach, because coach doesn't need this profile) and created decorator requiring "coach" role to use creating functions.

--- team/models.py
+++ team/models.py
@@ -1,25 +1,54 @@
 from team import db
-from flask_login import UserMixin
+from flask_login import UserMixin, current_user
+from functools import wraps
 
 
-class Player(db.Model, UserMixin):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email_address = db.Column(db.String(60), unique=True)
-    password_hash = db.Column(db.String(60))
-    first_name = db.Column(db.String(60))
-    last_name = db.Column(db.String(60))
-    birth_date = db.Column(db.Date)
+    email_address = db.Column(db.String(60), unique=True, nullable=False)
+    password_hash = db.Column(db.String(60), nullable=False)
+    user_type = db.Column(db.String(10), default="player")
+    profile = db.relationship('PlayerProfile', backref='owner')
+
+    def is_coach(self):
+        return self.user_type == 'coach'
+
+
+attendance = db.Table('attendance',
+                      db.Column('profile_id', db.Integer, db.ForeignKey('profile.id'), primary_key=True),
+                      db.Column('meeting_id', db.Integer, db.ForeignKey('meeting.id'), primary_key=True),
+                      )
+
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(60), nullable=False)
+    last_name = db.Column(db.String(60), nullable=False)
+    birth_date = db.Column(db.Date, default=None)
     position = db.Column(db.String(3))
+    number = db.Column(db.String(2), unique=True)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    meeting_attendance = db.relationship('Meeting',
+                                         secondary=attendance,
+                                         backref='present',
+                                         lazy='dynamic')
 
 
-class Coach(db.Model, UserMixin):
+class Meeting(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email_address = db.Column(db.String(60), unique=True)
-    token = db.Column(db.String(4))
-    password_hash = db.Column(db.String(60))
+    date = db.Column(db.Date)
+    attend = db.relationship('PlayerProfile', secondary=attendance, backref='present_players')
 
 
+def usertype_required(user_type):
+    def wrapper(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            if current_user.user_type != user_type:
+                # Redirect the user to an unauthorized notice!
+                return "You are not authorized to access this page"
+            return f(*args, **kwargs)
 
+        return wrapped
 
-
-
+    return wrapper
