7d3622c56f1f6ba1af8c64f6e6dbb148ed759cb3
Adrian Buśkiewicz <adrian.k.buskiewicz@gmail.com>
2021-08-02 11:47:52 +0200

    Created basic views to be developed

--- team/views.py
+++ team/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-
+from team.models import usertype_required
 
 views = Blueprint('views', __name__, url_prefix='/')
 
@@ -8,3 +8,30 @@ views = Blueprint('views', __name__, url_prefix='/')
 @views.route('/home')
 def home_page():
     return render_template('views/home.html')
+
+
+@views.route('/squad')
+def squad_page():
+    return render_template('views/squad.html')
+
+
+@views.route('/attendance')
+def attendance_page():
+    return render_template('views/attendance.html')
+
+
+@views.route('/player')
+def player_profile_page():
+    return render_template('views/player_profile.html')
+
+
+@views.route('/change-attendance')
+@usertype_required('coach')
+def change_attendance_page():
+    return render_template('views/change_attendance.html')
+
+
+@views.route('/create-meeting')
+@usertype_required('coach')
+def create_meeting_page():
+    return render_template('views/create_meeting.html')
