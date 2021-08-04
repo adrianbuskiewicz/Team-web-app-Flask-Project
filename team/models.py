8153fb5f1ad1f00111d7f6e58550aabcfdb28478
Adrian Buśkiewicz <adrian.k.buskiewicz@gmail.com>
2021-08-05 00:30:50 +0200

    Cosmetic changed to models

--- team/models.py
+++ team/models.py
@@ -1,14 +1,15 @@
 from team import db
 from flask_login import UserMixin, current_user
 from functools import wraps
+from flask import flash, redirect, url_for
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email_address = db.Column(db.String(60), unique=True, nullable=False)
-    password_hash = db.Column(db.String(60), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email_address = db.Column(db.String(60), unique=True)
+    password_hash = db.Column(db.String(60))
     user_type = db.Column(db.String(10), default="player")
-    profile = db.relationship('PlayerProfile', backref='owner')
+    profile = db.relationship('Profile', backref='owner', uselist=False)
 
     def is_coach(self):
         return self.user_type == 'coach'
@@ -25,19 +26,23 @@ class Profile(db.Model):
     first_name = db.Column(db.String(60), nullable=False)
     last_name = db.Column(db.String(60), nullable=False)
     birth_date = db.Column(db.Date, default=None)
-    position = db.Column(db.String(3))
+    position = db.Column(db.String(20))
     number = db.Column(db.String(2), unique=True)
-    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     meeting_attendance = db.relationship('Meeting',
                                          secondary=attendance,
-                                         backref='present',
                                          lazy='dynamic')
 
 
 class Meeting(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    attend = db.relationship('PlayerProfile', secondary=attendance, backref='present_players')
+    hour = db.Column(db.String(10))
+    day = db.Column(db.String(15))
+    type = db.Column(db.String(20))
+    locality = db.Column(db.String(50))
+    pitch = db.Column(db.String(30))
+    attendance = db.relationship('Profile', secondary=attendance)
 
 
 def usertype_required(user_type):
@@ -46,9 +51,8 @@ def usertype_required(user_type):
         def wrapped(*args, **kwargs):
             if current_user.user_type != user_type:
                 # Redirect the user to an unauthorized notice!
-                return "You are not authorized to access this page"
+                flash("You are not a coach!")
+                return redirect(url_for('views.home_page'))
             return f(*args, **kwargs)
-
         return wrapped
-
     return wrapper
