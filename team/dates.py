3b99e153719601185953f56f47c4eaf515e04c92
Adrian Buśkiewicz <adrian.k.buskiewicz@gmail.com>
2021-08-05 00:29:20 +0200

    New module with important date aspects

---
+++ team/dates.py
@@ -0,0 +1,7 @@
+from datetime import date, timedelta
+
+today = date.today()
+start_of_week = today - timedelta(days=today.weekday())
+end_of_week = start_of_week + timedelta(days=6)
+
+weekdays_tuple = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
