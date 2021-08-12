a76f3ec49d85a30c38a446e8c39f47b2078a8661
Adrian Buśkiewicz <adrian.k.buskiewicz@gmail.com>
2021-08-12 03:16:32 +0200

    Update creator.py
    Changed updated process to the old version

--- team/creator.py
+++ team/creator.py
@@ -5,14 +5,16 @@ from flask import (
     url_for,
     flash,
     request)
-from team.models import User, Profile, Meeting, usertype_required, delete_object
+from team.models import User, Profile, Meeting, usertype_required, delete_object, add_to_table
 from team.forms import (
     AddPlayerForm,
     CreateMeetingForm,
     UpdatePlayerForm,
     UpdateMeetingForm,
+    UpdateAttendanceForm,
     DeleteForm,
-    form_errors)
+    form_errors,
+    pitch_positions)
 from team import db
 from flask_login import login_required
 from werkzeug.security import generate_password_hash
@@ -50,30 +52,24 @@ def players_page():
         return redirect(url_for('creator.players_page'))
 
     if update_player_form.submit_update.data:
-        # profile_to_update = request.form.get('update_player')
-        # update_profile = Profile.query.filter_by(profile_id=profile_to_update).first()
-        # if update_profile:
-        #     update_profile.first_name = update_player_form.first_name.data
-        #     update_profile.last_name = update_player_form.last_name.data
-        #     update_profile.position = update_player_form.position.data
-        #     update_profile.number = update_player_form.number.data
-        #     update_profile.birth_date = update_player_form.birth_date.data
-        #     db.session.commit()
-        update_id = request.form.get('update_player')
-        profile_to_update = Profile.query.filter_by(profile_id=update_id).first()
-        update_player_form.obj = profile_to_update
-        if profile_to_update:
-            update_player_form.populate_obj(profile_to_update)
+        profile_to_update = request.form.get('update_player')
+        update_profile = Profile.query.filter_by(profile_id=profile_to_update).first()
+        if update_profile:
+            update_profile.first_name = update_player_form.first_name.data
+            update_profile.last_name = update_player_form.last_name.data
+            update_profile.position = update_player_form.position.data
+            update_profile.number = update_player_form.number.data
+            update_profile.birth_date = update_player_form.birth_date.data
             db.session.commit()
             flash("Changed player!", category='info')
             return redirect(url_for('creator.players_page'))
 
     if delete_player_form.submit_delete.data and delete_player_form.validate():
         delete_object(request_val='delete_player',
-                      table=Profile,
-                      obj_id='owner_id')
+                      model=Profile,
+                      obj_id='user_id')
         delete_object(request_val='delete_player',
-                      table=User,
+                      model=User,
                       obj_id='user_id')
         flash("Deleted player!", category='info')
         return redirect(url_for('creator.players_page'))
@@ -97,7 +93,7 @@ def meetings_page():
 
     if create_meeting_form.submit_create.data and create_meeting_form.validate():
         meeting_to_create = Meeting(date=create_meeting_form.date.data,
-                                    hour=create_meeting_form.hour.data,
+                                    hour=str(create_meeting_form.hour.data)[:5],
                                     day=weekdays_tuple[create_meeting_form.date.data.weekday()],
                                     type=create_meeting_form.type.data,
                                     locality=create_meeting_form.locality.data,
@@ -110,21 +106,20 @@ def meetings_page():
     if update_meeting_form.submit_update.data:
         update_id = request.form.get('update_meeting')
         meeting_to_update = Meeting.query.filter_by(meeting_id=update_id).first()
-        update_meeting_form.obj = meeting_to_update
         if meeting_to_update:
-            # meeting_to_update.date = update_meeting_form.date.data
-            # meeting_to_update.hour = update_meeting_form.hour.data
-            # meeting_to_update.locality = update_meeting_form.locality.data
-            # meeting_to_update.type = update_meeting_form.type.data
-            # meeting_to_update.pitch = update_meeting_form.pitch.data
-            update_meeting_form.populate_obj(meeting_to_update)
+            meeting_to_update.date = update_meeting_form.date.data
+            meeting_to_update.hour = str(update_meeting_form.hour.data)[:5]
+            meeting_to_update.day = weekdays_tuple[create_meeting_form.date.data.weekday()]
+            meeting_to_update.locality = update_meeting_form.locality.data
+            meeting_to_update.type = update_meeting_form.type.data
+            meeting_to_update.pitch = update_meeting_form.pitch.data
             db.session.commit()
             flash("Changed meeting!", category='info')
             return redirect(url_for('creator.meetings_page'))
 
     if delete_meeting_form.submit_delete.data and delete_meeting_form.validate():
         delete_object(request_val='delete_meeting',
-                      table=Meeting,
+                      model=Meeting,
                       obj_id='meeting_id')
         flash("Deleted meeting!", category='info')
         return redirect(url_for('creator.meetings_page'))
@@ -137,34 +132,27 @@ def meetings_page():
                            meetings=meetings)
 
 
-@creator.route('/attendance/update/<meeting_id>', methods=['GET'])
+@creator.route('/attendance/update/<meeting_id>', methods=['GET', 'POST'])
 @login_required
 @usertype_required('player')
 def update_attendance_page(meeting_id):
-    positions = ('goalkeeper',
-                 'defender',
-                 'midfielder',
-                 'forward')
+    positions = pitch_positions
+    update_attendance_form = UpdateAttendanceForm()
     meeting = Meeting.query.filter_by(meeting_id=meeting_id).first()
     players = Profile.query.all()
-    return render_template('creator/attendance_update.html', meeting=meeting, players=players, positions=positions)
+    if update_attendance_form.submit_update.data:
+        player_present_id = request.form.get('present_player')
+        player_present = Profile.query.filter_by(profile_id=player_present_id).first()
+        if meeting.attendance.filter_by(profile_id=player_present_id).first():
+            meeting.attendance.remove(player_present)
+            db.session.commit()
+        else:
+            meeting.attendance.append(player_present)
+            db.session.commit()
+        return redirect(url_for('creator.update_attendance_page', meeting_id=meeting.meeting_id))
 
-
-@creator.route("/attendance/update/<meeting_id>/<player_id>", methods=['POST'])
-@login_required
-def like(meeting_id, player_id):
-    meeting = Meeting.query.filter_by(meetingid=meeting_id).first()
-    pass
-    # like = Like.query.filter_by(author=current_user.id, post_id=post_id).first()
-    #
-    # if not post:
-    #     return jsonify({'error': 'Post does not exist.'}, 400)
-    # elif like:
-    #     db.session.delete(like)
-    #     db.session.commit()
-    # else:
-    #     like = Like(author=current_user.id, post_id=post_id)
-    #     db.session.add(like)
-    #     db.session.commit()
-    #
-    # return jsonify({"likes": len(post.likes), "liked": current_user.id in map(lambda x: x.author, post.likes)})
+    return render_template('creator/attendance_update.html',
+                           update_attendance_form=update_attendance_form,
+                           meeting=meeting,
+                           players=players,
+                           positions=positions)
